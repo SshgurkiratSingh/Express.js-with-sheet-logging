718ddcb837e695336bd04da4e9c252d7252f468a
Gurkirat <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-05-01 22:47:32 +0530

    crone:addedhistoryandlive

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ const fs = require("fs");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
 app.get("/api/get", (req, res) => {
   let jsonData = fs.readFileSync("data.json");
   res.json(JSON.parse(jsonData));
 });
+app.get("/api/getspecific/:id", (req, res) => {
+  let nn = req.params.id;
+  let jsonData = JSON.parse(fs.readFileSync("data.json"));
+  let newdata = jsonData.map((item) => ({
+    value: item[nn],
+    date: item.date,
+  }));
+
+  res.json(newdata);
+});
+
 app.get("/api/latest", (req, res) => {
   let jsonData = JSON.parse(fs.readFileSync("data.json"));
   res.json(jsonData[jsonData.length - 1]);
@@ -27,5 +44,14 @@ app.post("/api/add", (req, res) => {
   fs.writeFileSync("data.json", JSON.stringify(data));
   res.status(200).json({ file: "done" });
 });
+app.get("/specific/:aa", (req, res) => {
+  let nn = req.params.aa;
+  let jsonData = JSON.parse(fs.readFileSync("data.json"));
+  let newdata = jsonData.map((item) => ({
+    value: item[nn],
+    date: item.date,
+  }));
 
+  res.render("table.ejs", { data: newdata, name: nn });
+});
 app.listen(3000, () => {});
