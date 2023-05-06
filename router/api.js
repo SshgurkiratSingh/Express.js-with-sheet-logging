df9418e12ab18a6979229706e01eb1394b85c556
Gurkirat <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-05-06 18:27:01 +0530

    crone

--- router/api.js
+++ router/api.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-router.get("/", (req, res) => {
-  res.json({ message: "Welcome to the API" });
-});
+
 router.use(express.json());
+
 router.use(express.urlencoded({ extended: false }));
 router.post("/custom", async (req, res) => {
   let jsonData = fs.readFileSync("customisation.json");
@@ -15,6 +14,13 @@ router.post("/custom", async (req, res) => {
   fs.writeFileSync("customisation.json", JSON.stringify(data));
 
   res.redirect("/");
+});
+router.get("/", (req, res) => {
+  res.json({ message: "Welcome to the API" });
+});
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "Welcome to the API", date: new Date() });
 });
 router.post("/customHeading", async (req, res) => {
   let jsonData = fs.readFileSync("customisation.json");
