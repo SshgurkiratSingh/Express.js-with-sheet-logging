a070d1a5986b0c0587d8550bcbbdbf5abe069d41
Gurkirat <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-05-29 17:29:08 +0530

    feat:list

--- router/api.js
+++ router/api.js
@@ -23,58 +23,9 @@ router.post("/add/:node", async (req, res) => {
   res.json(dataToStore);
 });
 
-router.post("/api/add", async (req, res) => {
-  const jsonData = await fs.readFile(dataFile, "utf8");
-  const data = JSON.parse(jsonData);
-  const dataToStore = {
-    date: new Date(),
-    value1: req.body.value1,
-    value2: req.body.value2,
-    value3: req.body.value3,
-    value4: req.body.value4,
-  };
-  data.push(dataToStore);
-  await fs.writeFile(dataFile, JSON.stringify(data));
-  // Stein API Here **************************
-  // const store = new SteinStore("https://api.steinhq.com/v1/storages/6453d2d8eced9b09e9cdd875");
-  // store.append("Sheet1", [dataToStore]).then((res) => {
-  //   console.log(res);
-  // });
-  // res.status(200).json({ file: "done" });
-});
-
-router.post("/custom", async (req, res) => {
-  const jsonData = await fs.readFile(customisationFile, "utf8");
-  const data = JSON.parse(jsonData);
-  const { select, title, subtitle, description, unit, guage, maxValue } =
-    req.body;
-  const sensor = data.sensor[select];
-  sensor.title = title;
-  sensor.subtitle = subtitle;
-  sensor.description = description;
-  sensor.unit = unit;
-  sensor.guage = guage;
-  sensor.maxValue = maxValue;
-  await fs.writeFile(customisationFile, JSON.stringify(data));
-  res.redirect("/");
-});
-
-router.get("/latest", async (req, res) => {
-  const jsonData = await fs.readFile(dataFile, "utf8");
-  const data = JSON.parse(jsonData);
-  const node1Latest = data["node1"][data["node1"].length - 1];
-  const node2Latest = data["node2"][data["node2"].length - 1];
-  res.json({
-    value1: node1Latest.val1,
-    value2: node1Latest.val2,
-    value3: node2Latest.val1,
-    value4: node2Latest.val2,
-    date1: node1Latest.date,
-    date2: node2Latest.date,
-  });
-});
 router.get("/history/:node", async (req, res) => {
   const { node } = req.params;
+  const { page, limit } = req.query;
   const jsonData = await fs.readFile(dataFile, "utf8");
   const data = JSON.parse(jsonData);
   let selectedData = [];
@@ -104,12 +55,20 @@ router.get("/history/:node", async (req, res) => {
         date: new Date(date).toLocaleString(),
       })) || [];
   }
+  selectedData = selectedData.reverse();
 
-  // Get the last 15 values from selectedData
-  selectedData = selectedData.slice(-25);
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const totalPages = Math.ceil(selectedData.length / limit);
 
-  res.json(selectedData);
+  const paginatedData = selectedData.slice(startIndex, endIndex);
+  
+  res.json({
+    data: paginatedData,
+    totalPages: totalPages,
+  });
 });
+
 
 router.post("/customHeading", async (req, res) => {
   const jsonData = await fs.readFile(customisationFile, "utf8");
