7b33639f268f1ec7d27890f61d8a991bba0b5439
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-09-02 21:02:48 +0530

    Update ArduinotoNode.ino

--- ArduinotoNode/ArduinotoNode.ino
+++ ArduinotoNode/ArduinotoNode.ino
@@ -2,10 +2,10 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
-const char *ssid = "Node ";
-const char *password = "whyitellyou";
+const char *ssid = "ConForNode";
+const char *password = "12345678";
 
-const char *serverUrl = "http://192.168.1.100:3000/api/add/node2"; // Server Url to Post
+const char *serverUrl = "https://expressjs-with-sheet-logging.gurkirat7092.repl.co/api/add/node1"; // Server Url to Post
 
 void setup()
 {
@@ -22,7 +22,6 @@ void setup()
 
 void loop()
 {
-
   int value1 = random(100); // random value between 0 and 100
   int value2 = random(100); // random value between 0 and 100
 
@@ -30,20 +29,25 @@ void loop()
   payload["value1"] = value1;        // add the value to the payload
   payload["value2"] = value2;        // add the value to the payload
 
-  String jsonPayload;                  // String to hold the JSON payLoad
-  serializeJson(payload, jsonPayload); // converting the json to string
+  String jsonPayload;                  // String to hold the JSON payload
+  serializeJson(payload, jsonPayload); // converting the JSON to a string
 
-  WiFiClient client;
+  WiFiClientSecure client;
   HTTPClient http; // create the http client object
 
-  http.begin(client, serverUrl);                      // connect to the server
+  client.setInsecure(); // Ignore SSL certificate verification if self-signed
+
+  http.begin(client, serverUrl); // connect to the server
   http.addHeader("Content-Type", "application/json"); // set the content type header
-  int httpResponseCode = http.POST(jsonPayload);      // send the request
-  http.end();                                         // close the connection
+
+  Serial.println("Sending HTTP POST request...");
+  int httpResponseCode = http.POST(jsonPayload); // send the request
 
   Serial.print("HTTP response code: ");
   Serial.println(httpResponseCode);
   Serial.println(jsonPayload);
+
+  http.end();
 
   delay(5000);
 }
